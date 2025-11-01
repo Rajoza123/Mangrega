95447368b4b4bd3c91d3c952ed844f96738215ee
Rajoza123 <ozaraj76@gmail.com>
2025-11-01 23:03:51 +0530

    components changes in frontend

---
+++ frontend/src/components/TrendSparkline.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrendSparkline = ({ data }) => {
+  return (
+    <div style={{ width: "100%", height: "40px", background: "#f0f0f0", textAlign: "center", lineHeight: "40px" }}>
+      Trend Chart (coming soon)
+    </div>
+  );
+};
+
+export default TrendSparkline;
